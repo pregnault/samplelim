d111ee1ad56f7092c9a0dc918a6565f8b05a1c5a
Philippe Regnault <philippe.regnault@univ-reims.fr>
2025-03-13 11:31:01 +0100

    Fixed warning message due to misspecification of print method export. Updated general documentation file of samplelim.

--- R/plot_lim.R
+++ R/plot_lim.R
@@ -1,52 +1,53 @@
 #' Plot a metabolic network
 #'
-#' @param lim A list of class `lim` containing a component `Flowmatrix`.
+#' @param x A list of class `lim` containing a component `Flowmatrix`.
 #' @param seed The seed for the PRNG of the R session
-#' @param ... Additionnal arguments to be passed to the function `plot.igraph()` from the package `{igraph}`. 
+#' @param ... Additionnal arguments to be passed to the function \code{plot.igraph()} from the package \code{\\{igraph\\}}. 
 #'
 #' @return Returns NULL, invisibly.
 #' @importFrom igraph graph_from_adjacency_matrix
 #' @importFrom igraph plot.igraph
+#' @method plot lim
 #' @export
 #'
 #' @examples
 #' DF <- system.file("extdata", "DeclarationFileBOWF-short.txt", package = "samplelim")
 #' BOWF <- df2lim(DF)
 #' plot(BOWF)
-plot.lim <- function(lim, seed=NULL, ...) {
+plot.lim <- function(x, seed=NULL, ...) {
   # Check if 'Flowmatrix' exists in the object 'lim'
-  if (!exists("Flowmatrix", where = lim)) {
+  if (!exists("Flowmatrix", where = x)) {
     stop("'Flowmatrix' does not exist in used 'lim' object.")
   }
   
   # Check if 'Flowmatrix' is a matrix
-  if (!is.matrix(lim$Flowmatrix)) {
+  if (!is.matrix(x$Flowmatrix)) {
     stop("'Flowmatrix' from 'lim' object is not a matrix. ")
   }
   
   # Check if the dimensions of the matrix are equal to the expected dimensions
   # As dimensions depends of NComponents and NExternal we first check if they exist and are integers
-  if (!exists("NComponents", where = lim)){
+  if (!exists("NComponents", where = x)){
     stop("'NComponents' does not exist in used 'lim' object.")
   }
   
-  if (!is.integer(lim$NComponents)){
+  if (!is.integer(x$NComponents)){
     stop("'NComponents' from 'lim' object must be an integer.")
   }
   
-  if (!exists("NExternal", where = lim)){
+  if (!exists("NExternal", where = x)){
     stop("'NExternal' does not exist in used 'lim' object.")
   }
   
-  if (!is.integer(lim$NExternal)){
+  if (!is.integer(x$NExternal)){
     stop("'NExternal' from 'lim' object must be an integer.")
   }
   
   # Calculate the number of nodes
-  N_nodes <- lim$NComponents + lim$NExternal
+  N_nodes <- x$NComponents + x$NExternal
   
   expected_dim <- c(N_nodes, N_nodes)
-  if (all(dim(lim$Flowmatrix) != expected_dim)) {
+  if (all(dim(x$Flowmatrix) != expected_dim)) {
     stop("Flowmatrix from 'lim' object does not match the expected dimension.")
   }
 
@@ -55,7 +56,7 @@ plot.lim <- function(lim, seed=NULL, ...) {
     set.seed(seed) 
   }
 
-  adjacency_matrix <- as.matrix(lim$Flowmatrix > 0)
+  adjacency_matrix <- as.matrix(x$Flowmatrix > 0)
   model_graph <- graph_from_adjacency_matrix(adjacency_matrix)
   plot.igraph(model_graph, ...)
 
